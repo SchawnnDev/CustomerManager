4368bb2f1976933284fdcfb324ccc3aae484e9ab
SchawnnDev <iperskill@gmail.com>
2018-10-02 17:51:05 +0200

    started coding

---
+++ CustomerManager/Customer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerManager
+{
+    class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; private set; }
+        public string Name { get; private set; }
+        public DateTime DateOfBirth { get; private set; }
+        public string PhoneNumber { get; private set; }
+        public string Email { get; private set; }
+
+        public Customer(string firstName, string name, DateTime dateOfBirth, string phoneNumber, string email)
+        {
+            FirstName = firstName;
+            Name = name;
+            DateOfBirth = dateOfBirth;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+    }
+
+    class CustomerShippingAddress
+    {
+
+        public int CustomerId { get; set; }
+        public string Address { get; private set; }
+        public string PostalCode { get; private set; }
+
+        public CustomerShippingAddress(int customerId, string address, string postalCode)
+        {
+            CustomerId = customerId;
+            Address = address;
+            PostalCode = postalCode;
+        }
+    }
+
+}
--- CustomerManager/Program.cs
+++ CustomerManager/Program.cs
@@ -1,15 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace CustomerManager
 {
     class Program
     {
+
+        public static List<Customer> Customers = new List<Customer>();
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Loading app CustomerManager...");
+
+            DBManager.Init();
+
+            DBManager.LoadData();
+
+            string[] customers = ReadFile(args[0]).Skip(1).ToArray();
+            
+            foreach(string customer in customers)
+            {
+                string[] infos = customer.Split(',');
+                Customers.Add(new Customer(infos[0], infos[1], DateTime.Parse(infos[2]), infos[3], infos[4]));
+                
+            }
+
         }
+
+        private static string[] ReadFile(string path)
+        {
+            return File.ReadAllLines(path);
+        }
+
     }
+
 }
